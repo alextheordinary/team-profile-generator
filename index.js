153deb0ed9347d3caa5701c8bbde55d2a9b0072b
Alex Bellamy <alexjbellamy@gmail.com>
2022-09-10 14:51:52 -0400

    added addEngineer, addIntern, callMenuPrompt functions

--- index.js
+++ index.js
@@ -5,38 +5,167 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 let teamManager;
+let teamMembers = [];
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: `What is the team manager's name?`,
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: `What is the team manager's employee ID?`,
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: `What is the team manager's email address?`,
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: `What is the team manager's office number?`,
-            name: 'officeNumber'
-        },
-    ])
-    .then((answers) => {
-        teamManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-        console.log(teamManager);
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else went wrong
-        }
-    });
+// Solicits information about the team manager and then creates a new manager object after validating inputs
+function setupManager() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: `What is the team manager's name?`,
+                name: 'name'
+            },
+            {
+                type: 'number',
+                message: `What is the team manager's employee ID?`,
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: `What is the team manager's email address?`,
+                name: 'email'
+            },
+            {
+                type: 'number',
+                message: `What is the team manager's office number?`,
+                name: 'officeNumber'
+            },
+        ])
+        .then((answers) => {
+            teamManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            console.log(teamManager);
+            callMenuPrompt();
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
+
+// Brings up the prompt to solicit Engineer information and then add them to the team
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: `Engineer name?`,
+                name: 'name'
+            },
+            {
+                type: 'number',
+                message: `Employee ID?`,
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: `Email address?`,
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: `Github username?`,
+                name: 'github'
+            },
+        ])
+        .then((answers) => {
+            let newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            teamMembers.push(newEngineer);
+            // console.log(teamMembers);
+            callMenuPrompt();
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
+
+// Brings up the prompt to solicit Intern information and then add them to the team
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: `Intern name?`,
+                name: 'name'
+            },
+            {
+                type: 'number',
+                message: `Employee ID?`,
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: `Email address?`,
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: `School name?`,
+                name: 'school'
+            },
+        ])
+        .then((answers) => {
+            let newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            teamMembers.push(newIntern);
+            // console.log(teamMembers);
+            callMenuPrompt();
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
+
+// Brings up the prompt either add an engineer, add an intern, or publish the page
+function callMenuPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: `Choose action:`,
+                name: 'action',
+                choices: [
+                    { name: "Add an engineer", value: "Engineer" },
+                    { name: "Add an intern", value: "Intern" },
+                    { name: "Publish HTML Page", value: "Publish" },
+                ]
+            },
+        ])
+        .then((answers) => {
+            console.log(answers.action);
+            switch (answers.action) {
+                case "Engineer": {
+                    addEngineer();
+                    break;
+                }
+                case "Intern": {
+                    addIntern();
+                    break;
+                }
+                case "Publish": {
+
+                    break;
+                }
+
+            }
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
+
+setupManager();
