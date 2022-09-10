a234ac7fe60f64945d9fbf6a2ddddc820d7d0198
Alex Bellamy <alexjbellamy@gmail.com>
2022-09-10 10:31:07 -0400

    implemented tests for remaining Employee functions

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -7,4 +7,25 @@ describe("Employee class", () => {
             expect(testEmployee.getName()).toBe('Steve');
         });
     });
+
+    describe("getId method", () => {
+        it("returns employee ID", () => {
+            const testEmployee = new Employee('Steve', 1, 'steve@steveco.com');
+            expect(testEmployee.getId()).toBe(1);
+        });
+    });
+
+    describe("getEmail method", () => {
+        it("returns employee email", () => {
+            const testEmployee = new Employee('Steve', 1, 'steve@steveco.com');
+            expect(testEmployee.getEmail()).toBe('steve@steveco.com');
+        });
+    });
+
+    describe("getRole method", () => {
+        it("returns employee name", () => {
+            const testEmployee = new Employee('Steve', 1, 'steve@steveco.com');
+            expect(testEmployee.getRole()).toBe('Employee');
+        });
+    });
 });
